ac9d186678dd25de4d939b3969f55d13cef633fd
nagalingachary <sivanagalingachary@gmail.com>
2023-08-24 06:51:06 +0530

    Added catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -67,7 +67,7 @@ cd /app &>>$LOGFILE
 
 VALIDATE $? "Moving into app directory"
 
-unzip /tmp/catalogue.zip &>>$LOGFILE
+jar xvf /tmp/catalogue.zip &>>$LOGFILE
 
 VALIDATE $? "unzipping catalogue"
 
